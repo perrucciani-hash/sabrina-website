e4ff8281f664587d214ec0a830ce9b01b25c4c99
perrucciani-hash <perrucciani@gmail.com>
2026-06-07 21:28:46 +0200

    width:163px;text-align:center;

--- header.js
+++ header.js
@@ -39,7 +39,9 @@ class SfHeader extends HTMLElement {
           background:#fff;color:#1a1a1a !important;
           padding:.6rem 1.4rem;border-radius:2px;font-weight:500;
           transition:background .25s,color .25s
-          min-width:150px;text-align:center;
+          white-space:nowrap;
+          width:163px;text-align:center;
+  
         }
         .menu .termin::after{display:none}
         .topbar.solid .menu .termin,
